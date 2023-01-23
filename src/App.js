919307ff5303617a8834b9a55f1cab1ff1aff580
szigszdavid <szigetkozi.szabo.david@gmail.com>
2023-01-23 19:52:36 +0100

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Animations from './pages/Animations/Animations';
+import GameTable from './pages/GameTable/GameTable';
 
 function App() {
   return (
     <div className="App">
       <Animations />
+      
     </div>
   );
 }
