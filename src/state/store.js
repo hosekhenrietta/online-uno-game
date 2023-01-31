1ee643b716cf33340f51a326685143b137d49b9e
Hosek Henrietta <v09mtn@inf.elte.hu>
2023-01-31 09:27:24 +0100

    createCards

--- src/state/store.js
+++ src/state/store.js
@@ -9,11 +9,9 @@ export function initStore() {
             "currentPlayerID": -1,
             "currentStateID": -1, // -1 - nem kezdodott 0- betolt 1- csempehuzas 2-lerakom 3-ellorzes(átsiklik,pontok,vége)
             "players": [],
-            "cards": [
-                
-                ["black",1],["black",2],["black",3],["red",4],["red",5]
-            ],
+            "cards": [],
             // 10-kimarad, 11-sorrendváltozás, 12-+2
+            //black: 1-sima. 2-+2, 3-+4
         },
         synced: false,
         clients: [],
@@ -22,17 +20,18 @@ export function initStore() {
 }
 
 export function createCards() {
-    for (let index = 0; index < 4; index++) {
-        if (index = 0) {
-           // color_="red"
-           tmp= [["red",1],["red",2],["red",3],["red",4],["red",5],["red",6],["red",7],["red",8],["red",9],["red",10],["red",11],["red",12],
-                ["red",1],["red",2],["red",3],["red",4],["red",5],["red",6],["red",7],["red",8],["red",9],["red",10],["red",11],["red",12],
-                ["red",1],["red",2],["red",3],["red",4],["red",5],["red",6],["red",7],["red",8],["red",9],["red",10],["red",11],["red",12],
-                ["red",1],["red",2],["red",3],["red",4],["red",5],["red",6],["red",7],["red",8],["red",9],["red",10],["red",11],["red",12],
-        ];
-            }
-       // state.clients.push({ color: color_, number: "" }); 
+
+    for (let db = 0; db < 3; db++) {
+        for (let id = 1; id <= 12; id++) {
+            state.game.cards.push(["red", id], ["blue", id], ["green", id], ["yellow", id]);
+        }
+
+        for (let id = 1; id <= 5; id++) {
+            state.game.cards.push(["black", id]);
+        }
     }
+
+    state.game.cards.sort(() => Math.random() - 0.5);
 }
 
 
