519de380add6bc01307385136d21a52623f01bc8
szigszdavid <szigetkozi.szabo.david@gmail.com>
2023-01-31 05:44:57 +0100

    animation fixed

--- src/pages/Animations/components/Box6.jsx
+++ src/pages/Animations/components/Box6.jsx
@@ -9,24 +9,35 @@ export default function Box6() {
   const [isAnimating, setIsAnimating] = useState(false);
   const redLineRef = useRef()
   const boxRef = useRef()
-  const [bosIsOverRedLine, setBoxIsOverRedLine] = useState(0)
+  const [boxIsOverRedLine, setBoxIsOverRedLine] = useState(false)
+  const [coordiante, setCoordiante] = useState(0)
+  const [coordianteHelper, setCoordianteHelper] = useState(-2000)
+  const [isCardSelected, setIsCardSelected] = useState(false)
 
     useEffect(() => {
-        console.log(bosIsOverRedLine);
-    }, [bosIsOverRedLine])
+        console.log(coordiante);
+        if(coordiante < 0)
+            setCoordianteHelper(coordianteHelper === 2010 ? 1990 :coordianteHelper + 10)
+    }, [coordiante])
 
     const handleOnDrag = (info) => {
+        
+        const helper = document.getElementById('kocka').getBoundingClientRect().top < 0
         console.log('====================================')
-        console.log(redLineRef.current.offsetTop + redLineRef.current.offsetHeight, document.getElementById('kocka').getBoundingClientRect())
+        console.log(helper)
         console.log('====================================')
-        if(document.getElementById('kocka').getBoundingClientRect().top < 0) {
-            console.log("if");
-            setBoxIsOverRedLine(-1000)
-        }
-        else {
-            console.log("else");
-            setBoxIsOverRedLine(0)
-        }
+        setCoordiante(document.getElementById('kocka').getBoundingClientRect().top)
+    }
+
+    const handleCardOnClick = () => {
+      if (isCardSelected === false) {
+        setIsCardSelected(true)
+        setCoordiante(-200)
+      }
+      else {
+        setCoordiante(-1000)
+        setIsCardSelected(false)
+      }
     }
 
 
@@ -36,72 +47,19 @@ export default function Box6() {
         <div className="hand-container">
       <div className="box-container">
         <motion.div
-          className="box"
-          animate={{
-            //x: 700,
-            //x: isAnimating ? 700 : 0,
-            opacity: isAnimating ? 1 : 0.5,
-            //backgroundColor: "blue",
-            //scale: 2
-            rotate: isAnimating ? 360 : 0,
-          }}
-          initial={{
-            opacity: 0.2,
-          }}
-          transition={{
-            /*type: "tween",
-            duration: 2*/
-            type: "spring",
-            stiffness: 100,
-            damping: 10, //100
-          }}
-          onClick={() => setIsAnimating(!isAnimating)}
-          whileHover={{
-            y: -200,
-          }}
-        ></motion.div>
-      </div>
-      <div className="box-container">
-        <motion.div
-          className="box"
-          animate={{
-            //x: 700,
-            //x: isAnimating ? 700 : 0,
-            opacity: isAnimating ? 1 : 0.5,
-            //backgroundColor: "blue",
-            //scale: 2
-            rotate: isAnimating ? 360 : 0,
-          }}
-          initial={{
-            opacity: 0.2,
-          }}
-          transition={{
-            /*type: "tween",
-            duration: 2*/
-            type: "spring",
-            stiffness: 100,
-            damping: 10, //100
-          }}
-          onClick={() => setIsAnimating(!isAnimating)}
-          whileHover={{
-            y: -200,
-          }}
-        ></motion.div>
-      </div>
-      <div className="box-container">
-        <motion.div
-        id="kocka"
-            ref={boxRef}
+            id="kocka"
           className="box"
           drag
-          onDragEnd={(event, info) => handleOnDrag(info)}
+          ref={boxRef}
+          //onDragEnd={(event, info) => handleOnDrag(info)}
           animate={{
-            //x: 700,
-            y: bosIsOverRedLine,
-            opacity: isAnimating ? 1 : 0.5,
-            //backgroundColor: "blue",
-            //scale: 2
-            //rotate: isAnimating ? 360 : 0,
+            y: coordiante
+          }}
+          onClick={() => handleCardOnClick()}
+          dragConstraints={{
+            left: 0,
+            right: 0,
+            bottom: 0,
           }}
           initial={{
             opacity: 0.2,
@@ -115,33 +73,6 @@ export default function Box6() {
           }}
           //onClick={() => setBoxIsOverRedLine(-200)}
           
-        ></motion.div>
-      </div>
-      <div className="box-container">
-        <motion.div
-          className="box"
-          animate={{
-            //x: 700,
-            //x: isAnimating ? 700 : 0,
-            opacity: isAnimating ? 1 : 0.5,
-            //backgroundColor: "blue",
-            //scale: 2
-            rotate: isAnimating ? 360 : 0,
-          }}
-          initial={{
-            opacity: 0.2,
-          }}
-          transition={{
-            /*type: "tween",
-            duration: 2*/
-            type: "spring",
-            stiffness: 100,
-            damping: 10, //100
-          }}
-          onClick={() => setIsAnimating(!isAnimating)}
-          whileHover={{
-            y: -200,
-          }}
         ></motion.div>
       </div>
     </div>
